0d040d0433c763fab389af9333de2a3d43fbb7f0
michaelamponsah <amponsahmichaelkwame@gmail.com>
2023-06-01 17:50:44 +0000

    Implement application Button UI Component

--- src/features/ui/button/index.jsx
+++ src/features/ui/button/index.jsx
@@ -2,20 +2,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Button = ({ type, text }) => (
-  <button type={type}>
-    {text}
-  </button>
-);
+const Button = ({
+  type, text, handleClick, btnColor,
+}) => {
+  const handleBtnClick = (e) => {
+    e.preventDefault();
+    handleClick();
+  };
+
+  return (
+    <button
+      className={`${btnColor} text-custom-light-50 text-white px-10 font-medium py-2 rounded`}
+      type={type}
+      onClick={handleBtnClick}
+    >
+      {text}
+    </button>
+  );
+};
 
 Button.propTypes = {
   type: PropTypes.string,
   text: PropTypes.string,
+  handleClick: PropTypes.func.isRequired,
+  btnColor: PropTypes.string,
 };
 
 Button.defaultProps = {
   type: 'button',
   text: 'Button',
+  btnColor: 'bg-custom-purple',
 };
 
 export default Button;
