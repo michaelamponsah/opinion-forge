b6e4a83d1a41627f9c6fd9165e186e55ed11f83d
michaelamponsah <amponsahmichaelkwame@gmail.com>
2023-06-01 17:20:38 +0000

    Extend tailwind.config.js to include custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,18 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors: {
+      'custom-purple': '#ad1fea',
+      'custom-dark-blue-50': '#647196',
+      'custom-dark-blue-100': '#3a4374',
+      'custom-dark-blue-200': '#4661e6',
+      'custom-light-50': '#ffffff',
+      'custom-light-100': '#f2f4ff',
+      'custom-light-150': '#f7f8fd',
+      'custom-warning-50': '#f49f85',
+      'custom-warning-100': '#d73737',
+      'custom-sea-blue': '#62bcfa',
+    },
   },
   plugins: [],
 };
