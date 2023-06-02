3fe9ffa1c73c21818ebbafd7c97dbe976927e7f6
michaelamponsah <amponsahmichaelkwame@gmail.com>
2023-06-02 17:13:09 +0000

    Test up-vote interactive element

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import { Button } from 'features/ui';
+// import { Button } from 'features/ui';
+// import { FaArrowLeft } from 'react-icons/fa';
+import { UpVote } from 'features/ui';
 
-const App = () => (
-  <div className="App">
-    <h1 className="text-3xl font-bold underline">Opinion Forge</h1>
-    <Button />
-  </div>
-);
+const App = () => {
+  const handleClickUpvote = (upvoteCount) => {
+    const { count, isActive } = upvoteCount;
 
+    const updatedUpvoteCount = {
+      count: isActive ? count - 1 : count + 1,
+      isActive: !isActive,
+    };
+
+    return updatedUpvoteCount;
+  };
+
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">Opinion Forge</h1>
+      <UpVote onClickUpvote={(upvoteCount) => handleClickUpvote(upvoteCount)} />
+    </div>
+  );
+};
 export default App;
