2c378c28b74419c7d6faccdb13cb6f6bf04d4dd6
michaelamponsah <amponsahmichaelkwame@gmail.com>
2023-06-03 00:14:29 +0000

    Implement navbar container and compose suggestion filtering component

---
+++ src/features/ui/navbar/index.jsx
@@ -0,0 +1,28 @@
+/* eslint-disable arrow-body-style */
+import React from 'react';
+import suggestionsIcon from '../../../assets/icons/icon-suggestions.svg';
+import SuggestionsFilter from './suggestions-filter';
+
+const Navbar = () => {
+  return (
+    <nav className="bg-custom-dark-blue-100 mx-auto px-4 py-5 rounded-lg">
+      <div className="container flex items-center">
+        <div className="flex items-center gap-10">
+          <div className="flex gap-4 font-medium text-lg text-custom-light-50">
+            <span>
+              <img src={suggestionsIcon} alt="a light bulb suggesing an idea" />
+            </span>
+            <span>
+              6 Suggestions
+            </span>
+          </div>
+          <div>
+            <SuggestionsFilter />
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
