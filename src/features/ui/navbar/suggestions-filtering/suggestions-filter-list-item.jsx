f8d43e8929bcb256225ed2ea1a4f4d4e73e5c87a
michaelamponsah <amponsahmichaelkwame@gmail.com>
2023-06-03 21:53:38 +0000

    Associate the SuggestionFilterListItem with a data-value prop

--- src/features/ui/navbar/suggestions-filtering/suggestions-filter-list-item.jsx
+++ src/features/ui/navbar/suggestions-filtering/suggestions-filter-list-item.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { PropTypes } from 'prop-types';
 
 const SuggestionsFilterListItem = ({ label }) => (
-  <li className="py-2 px-5">{label}</li>
+  <li data-value={label} className="py-2 px-5 text-slate-500 font-medium hover:cursor-pointer hover:text-custom-purple">{label}</li>
 );
 
 SuggestionsFilterListItem.propTypes = {
