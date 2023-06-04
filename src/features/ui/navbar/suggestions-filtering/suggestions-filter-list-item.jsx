14863ca5f278ab17bfc330b833f304169de35b58
michaelamponsah <amponsahmichaelkwame@gmail.com>
2023-06-04 20:56:36 +0000

    Implement suggestions sorting/filtering
    Users could select an option and get a visual feedback of selected item

--- src/features/ui/navbar/suggestions-filtering/suggestions-filter-list-item.jsx
+++ src/features/ui/navbar/suggestions-filtering/suggestions-filter-list-item.jsx
@@ -1,12 +1,54 @@
 import React from 'react';
 import { PropTypes } from 'prop-types';
+import { FaCheck } from 'react-icons/fa';
 
-const SuggestionsFilterListItem = ({ label }) => (
-  <li data-value={label} className="py-2 px-5 text-slate-500 font-medium hover:cursor-pointer hover:text-custom-purple">{label}</li>
-);
+const SuggestionsFilterListItem = ({
+  label,
+  selected,
+  onOptionChange,
+  onOptionFocus,
+}) => {
+  const handleChange = (e) => {
+    const itemValue = e.target.value;
+    onOptionChange(itemValue);
+  };
+
+  const handleFocus = () => {
+    onOptionFocus();
+  };
+
+  return (
+    <li
+      className="text-slate-500 font-medium hover:text-custom-purple flex"
+    >
+      <input
+        id={label}
+        type="radio"
+        name="suggestion-type"
+        value={label}
+        className="h-0 w-0"
+        checked={selected}
+        onChange={handleChange}
+        onFocus={handleFocus}
+      />
+      <label
+        htmlFor={label}
+        className="w-full inline-block py-2 px-5 hover:cursor-pointer"
+      >
+        {label}
+        <span>
+          {selected && <FaCheck />}
+        </span>
+      </label>
+    </li>
+  );
+};
 
 SuggestionsFilterListItem.propTypes = {
   label: PropTypes.string.isRequired,
+  onOptionChange: PropTypes.func.isRequired,
+  selected: PropTypes.bool.isRequired,
+  onOptionFocus: PropTypes.func.isRequired,
 };
 
 export default SuggestionsFilterListItem;
--- src/features/ui/navbar/suggestions-filtering/suggestions-filter-list.jsx
+++ src/features/ui/navbar/suggestions-filtering/suggestions-filter-list.jsx
@@ -1,38 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SuggestionsFilterListItem from './suggestions-filter-list-item';
 
 const filteringOptions = [
   {
-    key: 1,
+    key: 0,
     label: 'Most Upvotes',
   },
   {
-    key: 2,
+    key: 1,
     label: 'Least Upvotes',
   },
   {
-    key: 3,
+    key: 2,
     label: 'Most Comments',
   },
 
   {
-    key: 4,
+    key: 3,
     label: 'Least Comments',
   },
 
 ];
 
-const SuggestionsFilterList = () => (
-  <ul className="absolute top-24 bg-custom-light-50 drop-shadow-md py-1 w-1/5 rounded-lg divide-y-2 divide-slate-200">
-    {
-      filteringOptions.map((option) => (
-        <SuggestionsFilterListItem
-          key={option.key}
-          label={option.label}
-        />
-      ))
-    }
-  </ul>
-);
+const SuggestionsFilterList = () => {
+  const [selectedItem, setSelectedItem] = useState(0);
+
+  const handleOptionChange = (itemValue) => {
+    console.log('Selected: ', itemValue);
+  };
+
+  const handleOptionFocus = (index) => {
+    setSelectedItem(index);
+  };
+
+  return (
+    <ul className="absolute top-24 bg-custom-light-50 drop-shadow-md py-1 w-1/5 rounded-lg divide-y-2 divide-slate-200">
+      {
+    filteringOptions.map((option, index) => (
+      <SuggestionsFilterListItem
+        key={option.key}
+        label={option.label}
+        selected={selectedItem === index}
+        onOptionChange={(itemValue) => handleOptionChange(itemValue)}
+        onOptionFocus={() => handleOptionFocus(index)}
+      />
+    ))
+  }
+    </ul>
+  );
+};
 
 export default SuggestionsFilterList;
